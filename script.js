8d1b286d52a196cc7c4eed99a3094efb0bd617dc
1shomurodov <shomurotov.01112009@gmail.com>
2026-01-31 22:38:07 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const elements = document.querySelectorAll(".fade-in");
+  
+    // Hammasi birgalikda, lekin sekin-asta
+    elements.forEach(el => {
+      el.style.opacity = 0;
+      el.style.transform = "translateY(20px)";
+    });
+  
+    setTimeout(() => {
+      elements.forEach(el => {
+        el.style.transition = "opacity 1s ease, transform 1s ease";
+        el.style.opacity = 1;
+        el.style.transform = "translateY(0)";
+      });
+    }, 400); // 0.2s kechikishdan keyin hammasi asta-sekin chiqadi
+  });
+  
