3ab91b3dab6c3853249827e2af91e4b784f7f274
1shomurodov <shomurotov.01112009@gmail.com>
2026-02-01 15:52:32 +0500

    Update script.js

--- script.js
+++ script.js
@@ -15,4 +15,17 @@ document.addEventListener("DOMContentLoaded", () => {
       });
     }, 400); // 0.2s kechikishdan keyin hammasi asta-sekin chiqadi
   });
+// SEKIN CHIQIB KELISH EFFEKT
+document.addEventListener("DOMContentLoaded", () => {
+  const cards = document.querySelectorAll(".card");
+
+  cards.forEach((card, index) => {
+    setTimeout(() => {
+      card.style.transition = "1s ease";
+      card.style.opacity = "1";
+      card.style.transform = "translateY(0)";
+    }, index * 300);
+  });
+});
+
   
